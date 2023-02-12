f58d5be31fa8b58fbe70bc17b89e1fc3b570f9d4
maazahmad802 <maazch2020@gmail.com>
2023-02-12 10:24:16 -0800

    second js assignment

---
+++ script.js
@@ -0,0 +1,108 @@
+// problem#1
+// let name , admin;
+// name ="Umair Azmat";
+// admin = name;
+// alert(admin);
+
+// problem#2
+// let ourPlanetName = "Earth";
+// console.log(ourPlanetName);
+
+// // problem#3
+
+// let currentVisitor ="Maaz Ahmad";
+// console.log(currentVisitor);
+
+// // problem#4
+
+// let favDrink="Sting";
+// console.log(favDrink);
+
+// problem#5
+
+// let favNum ="07";
+// console.log(favNum);
+
+// problem#6
+// function myFavs() {
+//    let color, movie,food;
+//    color = "Black";
+//    movie = "KGF chapter 2";
+//    food = "Pizza";
+//    console.log(color,movie,food);
+   
+// }
+// myFavs();
+
+// problem#7 make object
+
+// let person ={
+//    personName:"Maaz Ahmad",
+//    age : 20,
+// };
+// console.log(person);
+
+// problem#8
+
+// let a , b ,sum;
+// a =10;
+// b =31;
+// sum=a+b;
+// console.log(sum);
+
+// problem#9
+
+// let a, b , Div;
+// a = 12;
+// b = 3;
+// Div = a/b;
+// console.log(Div);
+
+// problem#10
+ 
+// let a , b , sum , Div;
+// a = 32;
+// b = 18;
+// sum=a+b;
+// Div =sum/5;
+// console.log(Div);
+
+// problem#11 convert temp
+
+// function convertToCulsius(temp) {
+//    return(temp - 32) * 5/9;
+// }
+// let tempInF = 32;
+// let tempInC = convertToCulsius(tempInF);
+// console.log(tempInF + " F is equal to " +  tempInC);
+
+
+// problem#12 prompt value
+// let a, b,sum;
+//  a =parseFloat(prompt("Enter 1st number"));
+//  b =parseFloat(prompt("Enter 2nd number"));
+// sum =a +b;
+// alert(sum);
+// // problem#13
+// let a, b,diffr;
+//  a =parseFloat(prompt("Enter 1st number"));
+//  b =parseFloat(prompt("Enter 2nd number"));
+// diffr =a - b;
+// alert(diffr);
+
+
+// problem#14
+
+// let a, b,quotient;
+//  a =parseFloat(prompt("Enter 1st number"));
+//  b =parseFloat(prompt("Enter 2nd number"));
+// quotient =a/b;
+// alert(quotient);
+
+// problem#15
+
+let a, b,modulus;
+ a =parseFloat(prompt("Enter 1st number"));
+ b =parseFloat(prompt("Enter 2nd number"));
+modulus =a%b;
+alert(modulus);
